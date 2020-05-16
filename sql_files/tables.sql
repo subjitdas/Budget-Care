6ff1e0d88300986815b02a57b1a0be2c180e5f45
Subjit Das <subjitdas@gmail.com>
2020-05-16 12:35:45 +0530

    Rectified tables

--- sql_files/tables.sql
+++ sql_files/tables.sql
@@ -1,7 +1,3 @@
-
-#drop database budget_care;
-create database budget_care;
-
 Create table users
 (
 	username varchar(100) Primary key,
@@ -9,7 +5,7 @@ Create table users
 	first_name varchar(100) not null,
 	last_name varchar(100) not null,
 	created_at timestamp default CURRENT_TIMESTAMP,
-	monthly_budget decimal(10,2) not null
+	monthly_budget decimal(10,2) not null default 0
 );
 
 Create table spendings
@@ -17,6 +13,7 @@ Create table spendings
 	users_name varchar(100) not null,
 	event varchar(100) not null,
 	payment_method varchar(100) not null,
-	amount_spent decimal(10,2) not null,
+	amount_spent decimal(10,2) not null default 0,
+	created_at timestamp default CURRENT_TIMESTAMP,
 	foreign key(users_name) references users(username) on delete cascade
 );
