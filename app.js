546c44faf2bdcae37648c99fb04feaff1c2073eb
subjitdas <subjitdas@gmail.com>
2020-10-05 09:46:18 +0530

    Added helmet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyparser = require('body-parser')
 const routes = require('./routes/routes')
 const compression = require('compression')
+const helmet = require('helmet')
 
 const port = process.env.PORT||3000;
 
@@ -10,6 +11,7 @@ app.use(bodyparser.urlencoded({extended:true}))
 app.use(express.static(__dirname + "/public"))  //to connect to css folder
 app.use(routes)
 app.use(compression())
+app.use(helmet())
 
 app.set("view engine","ejs");
 
